a2d91a47186c3e16c3dce2fb971a30cde161fca7
HansaemYoo <77716290+HansaemYoo@users.noreply.github.com>
2021-01-22 13:46:19 +0900

    commit8
    EscapeExample

---
+++ EscapeExample.java
@@ -0,0 +1,12 @@
+package HelloJava;
+
+public class EscapeExample {
+
+	public static void main(String[] args) {
+		System.out.println("번호\t이름\t직업"); //\t는 탭만큼 띄움
+		System.out.println("행 단위 출력\n"); //\n은 줄바꿈
+		System.out.println("저는 \"개발자\"입니다."); //문자열안에 ""를 표시
+		System.out.println("봄\\여름\\가을\\겨울"); // \출력
+	}
+
+}

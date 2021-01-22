197ecdee2121de86c3166db411add75b69aedbce
HansaemYoo <77716290+HansaemYoo@users.noreply.github.com>
2021-01-22 18:51:55 +0900

    commit10
    논리 타입 booelan

---
+++ BooleanExample.java
@@ -0,0 +1,14 @@
+package HelloJava;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		boolean stop = true;
+		if(stop) { //참일때 실행할 내용
+			System.out.println("중지합니다.");
+		} else {	//거짓일 때 실행할 내용
+			System.out.println("시작합니다.");
+		}
+	}
+
+}

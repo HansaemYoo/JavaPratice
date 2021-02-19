bb72812049837ba9e2553bf0f802980222731836
HansaemYoo <77716290+HansaemYoo@users.noreply.github.com>
2021-02-19 11:33:10 +0900

    commit58
    final 필드 선언과 초기화

---
+++ Person.java
@@ -0,0 +1,27 @@
+package JavaPractice;
+
+public class Person {
+	
+	//final ÇÊµå ¼±¾ð°ú ÃÊ±âÈ­
+	final String nation = "Korea";
+	final String ssn;
+	String name;
+
+	public Person(String ssn, String name) {
+		this.ssn = ssn;
+		this.name = name;
+	}
+	
+	public static void main(String[] args) {
+
+		Person p1 = new Person("123456-1234567", "È«±æµ¿");
+		
+		System.out.println(p1.nation);
+		System.out.println(p1.ssn);
+		System.out.println(p1.name);
+		
+		p1.name = "È«»ï¿ø";
+		
+	}
+
+}
